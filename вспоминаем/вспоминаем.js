5df63644ee546ddd9aa51ae5d2e722d9f99c390d
IakovEU <kirbabin.jacob@gmail.com>
2022-08-25 17:32:54 +0300

    Взаимодействие с элементами с дома

--- вспоминаем/вспоминаем.js
+++ вспоминаем/вспоминаем.js
@@ -1069,3 +1069,60 @@ console.log(question); */
 //                                           // ~ Переменные не равны тк это лишь два хранилища с инфомацией~
 //  console.log(a == b);
 
+//------------------------------------------------------------------------------
+// const restorantData = {
+//     menu: [
+//         {
+//             name: 'Salad Caesar',
+//             price: '14$'
+//         },
+//         {
+//             name: 'Pizza Diavola',
+//             price: '9$'
+//         },
+//         {
+//             name: 'Beefsteak',
+//             price: '17$'
+//         },
+//         {
+//             name: 'Napoleon',
+//             price: '7$'
+//         }
+//     ],
+//     waitors: [
+//         {name: 'Alice', age: 22}, {name: 'John', age: 24}
+//     ],
+//     averageLunchPrice: '20$',
+//     openNow: true
+// };
+
+// function transferWaitors(data) {
+//     let copy = Object.assign({}, data);
+
+//     copy.waitors = [{name: 'Mike', age: 32}];
+//     return copy;
+// }
+
+// transferWaitors(restorantData);
+
+// console.log(transferWaitors(restorantData));
+
+// function isAverageLunchPriceTrue(fDish, sDish, average) {
+//     if (parseInt(fDish.price) + parseInt(sDish.price) < parseInt(average)) {
+//         return 'Цена ниже средней';
+//     } else {
+//         return 'Цена выше средней';
+//     }
+// }
+
+// console.log(isAverageLunchPriceTrue(restorantData.menu[3], restorantData.menu[1], restorantData.averageLunchPrice));
+
+// function isOpen(prop) {
+//     let answer = '';
+//     prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+//     return answer;
+// }
+
+// console.log(isOpen(restorantData.openNow));
+
